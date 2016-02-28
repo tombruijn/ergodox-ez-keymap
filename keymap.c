b0aeeb4544208500b47bd6d845260115eaafba86
Tom de Bruijn <tom@tomdebruijn.com>
2016-02-28 17:36:45 +0100

    Keymap update

--- keymap.c
+++ keymap.c
@@ -11,45 +11,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  |  -   |           | Del  |   6  |   7  |   8  |   9  |   0  |  BSPC  |
+ * |   =    |   1  |   2  |   3  |   4  |   5  |  -   |           |      |   6  |   7  |   8  |   9  |   0  |  BSPC  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | TAB    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \|   |
+ * | TAB    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L3  |   Y  |   U  |   I  |   O  |   P  |   \|   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | ESC    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '"   |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| BSPC |           | Del  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Grv  |      | Alt  | ~L3  | CMD  |                                       | ~L1  | ~L3  | Alt  |   [  |   ]  |
+ *   | Grv  |      | Alt  | ~L3  | CMD  |                                       | ~L1  | ~L3  | Alt  |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                        |      |      |       |      |        |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space| CMD  |------|       |------|  ~L1   |Enter |
- *                                 |      |      | Alt  |       | ~L1  |        |      |
+ *                                 |      |      |      |       |      |        |      |
+ *                                 | Space| Alt  |------|       |------|  CMD   |Enter |
+ *                                 |      |      |      |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
 [BASE] = KEYMAP(
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_MINS,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-        KC_ESC,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_GRV,         KC_NO,        KC_LALT,LT(MNGM, KC_NO), KC_LGUI,
+        KC_EQL,         KC_1,         KC_2,   KC_3,    KC_4,   KC_5,   KC_MINS,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,    KC_R,   KC_T,   TG(SYMB),
+        KC_ESC,         KC_A,         KC_S,   KC_D,    KC_F,   KC_G,
+        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,    KC_V,   KC_B,   KC_BSPC,
+        KC_GRV,         KC_NO,        KC_LALT,MO(MNGM),KC_LGUI,
 
-                                                       ALT_T(KC_APP), KC_LGUI,
-                                                                      KC_HOME,
-                                              KC_SPC,  KC_LGUI,       KC_LALT,
+                                                       KC_NO,   KC_NO,
+                                                                KC_NO,
+                                               KC_SPC, KC_LALT, KC_NO,
         // right hand
-        KC_DELETE,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
-        TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                     KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
-        MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                             LT(SYMB, KC_NO), LT(MNGM, KC_NO), KC_LALT,KC_LBRC,          KC_RBRC,
+        KC_NO,       KC_6,   KC_7,    KC_8,    KC_9,   KC_0,             KC_BSPC,
+        TG(MNGM),    KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,             KC_BSLS,
+                     KC_H,   KC_J,    KC_K,    KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
+        KC_DELETE,   KC_N,   KC_M,    KC_COMM, KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+                             MO(SYMB),MO(MNGM),KC_LALT,KC_NO,            KC_NO,
 
-        KC_LALT,      CTL_T(KC_ESC),
-        KC_PGUP,
-        ALL_T(KC_NO), KC_LALT, KC_ENT
+        KC_NO, KC_NO,
+        KC_NO,
+        KC_NO, KC_LGUI, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
